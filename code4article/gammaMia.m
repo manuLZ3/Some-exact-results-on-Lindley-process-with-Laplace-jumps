0faff5ac05778e04754cfe1236ceefb0465994cf
manuLZ3 <64789013+manuLZ3@users.noreply.github.com>
2023-03-13 23:45:51 +0100

    Update gammaMia.m
    removed comments

--- code4article/gammaMia.m
+++ code4article/gammaMia.m
@@ -6,13 +6,6 @@ function valore = gammaMia(a,b,p,s,k)
      Calcola l'integrale di ´exp(-x/s)*(x+k)^p´ tra ´a´ e ´b´.
      Con ´s=1´ e ´k=0´ si calcola la gamma incompleta
     %}
-
-    %{
-    valore = 0;
-    for d=0:p
-        valore= valore + factorial(p)/factorial(d)*s^(p-d+1)*( ( exp(-a/s)*(a+k)^(d) - exp(-b/s)*(b+k)^(d) ) );
-    end
-    %}
     estremo_superiore_integrale = evaluation_above(b,p,s,k);
     estremo_inferiore_integrale = evaluation_below(a,p,s,k);
     valore = estremo_superiore_integrale - estremo_inferiore_integrale;
